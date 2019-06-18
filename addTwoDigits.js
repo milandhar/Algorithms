79898a07d66e53cf1149dd391def849a8610101f
Milan <milan.dhar@gmail.com>
2019-06-18 09:24:39 -0700

    finished addTwo

---
+++ addTwoDigits.js
@@ -0,0 +1,33 @@
+function addTwoDigits(integer) {
+  numberString = integer.toString()
+  numberArray = numberString.split("")
+  return parseInt(numberArray[0]) + parseInt(numberArray[1])
+}
+
+// You are given a two-digit integer n. Return the sum of its digits.
+
+// Example
+//
+// For n = 29, the output should be addTwoDigits(n) = 11.
+//
+// Hint
+//
+// split()
+// parseInt()
+// toString()
+// reduce()
+// Input/Output
+//
+// [execution time limit] 4 seconds (js)
+//
+// [input] integer n
+//
+// A positive two-digit integer.
+//
+// Guaranteed constraints:
+//
+// 10 ≤ n ≤ 99.
+//
+// [output] integer
+//
+// The sum of the first and second digits of the input number.
